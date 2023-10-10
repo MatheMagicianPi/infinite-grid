ecce652aa35aca395a27c2bb2bcf7ea90a8d9638
Dang Phan <dangphan@uw.edu>
2023-10-09 21:11:08 -0700

    final

--- infinite_grid.py
+++ infinite_grid.py
@@ -6,11 +6,10 @@ import time
 # Define constants
 SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 600
-CELL_SIZE = 20  # Size of each cell
+CELL_SIZE = 10  # Size of each cell
 COLORS = {
-    0: (150, 150, 150),
-    1: (100, 100, 100),
-    2: (0, 0, 0)
+    0: (255, 255, 255),
+    1: (0, 0, 0),
 }
 
 # Initialize Pygame
@@ -21,13 +20,17 @@ pygame.display.set_caption("Infinite 2D Grid")
 # Initialize the grid with random colors
 grid_width = SCREEN_WIDTH // CELL_SIZE
 grid_height = SCREEN_HEIGHT // CELL_SIZE
-grid = np.random.choice([0, 1, 2], (grid_height, grid_width))
+grid = np.random.choice([0, 0, 0, 0, 0, 0, 0, 0, 0, 1], (grid_height, grid_width))
+new_grid = np.zeros((grid_height, grid_width))
+
+# grid[0][0] = 1
+# grid[0][1] = 1
+# grid[1][0] = 1
 
 def adjacent_neighbors(coords):
     neighbors = dict()
     neighbors[0] = []
     neighbors[1] = []
-    neighbors[2] = []
     row = coords[0]
     col = coords[1]
 
@@ -35,22 +38,25 @@ def adjacent_neighbors(coords):
         neighbors[(row - 1, col)] = None
     else:
         neighbors[(row - 1, col)] = grid[row - 1][col]
-        neighbors.get(grid[row - 1][col]).append((row - 1, col))
-    if col == grid_width - 1:
-        neighbors[(row, col - 1)] = None
-    else:
-        neighbors[(row, col - 1)] = grid[row][col - 1]
-        neighbors.get(grid[row][col - 1]).append((row, col - 1))
+        neighbors[grid[row - 1][col]].append((row - 1, col))
+
     if row == grid_height - 1:
         neighbors[(row + 1, col)] = None
     else:
         neighbors[(row + 1, col)] = grid[row + 1][col]
-        neighbors.get(grid[row + 1][col]).append((row + 1, col))
+        neighbors[grid[row + 1][col]].append((row + 1, col))
+
     if col == 0:
         neighbors[(row, col - 1)] = None
     else:
-        neighbors[(row, col - 1)] = grid[row][col - 1]
-        neighbors.get(grid[row][col - 1]).append((row, col - 1))
+        neighbors[(row, col - 1)] = grid[row, col - 1]
+        neighbors[grid[row][col - 1]].append((row, col - 1))
+
+    if col == grid_width - 1:
+        neighbors[(row, col + 1)] = None
+    else:
+        neighbors[(row, col + 1)] = grid[row, col + 1]
+        neighbors[grid[row][col + 1]].append((row, col + 1))
 
     return neighbors
 
@@ -60,37 +66,33 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    coordinates = (random.randint(0, grid_height - 1), random.randint(0, grid_width - 1))
-    cell = grid[coordinates[0]][coordinates[1]]
-    neighbors = adjacent_neighbors(coordinates)
-    if cell == 2:
-        if 2 in neighbors.values():
-            kill = random.choice(neighbors.get(2))
-            grid[kill[0]][kill[1]] = 0
-        elif 1 in neighbors.values() and 0 in neighbors.values():
-            child = random.choice(neighbors.get(0))
-            grid[child[0]][child[1]] = random.choice((1, 2))
-        elif 0 in neighbors.values():
-            move = random.choice(neighbors.get(0))
-            grid[move[0]][move[1]] = 1
-            grid[coordinates[0]][coordinates[1]] = 0
-    if cell == 1:
-        if 0 not in neighbors.values():
-            grid[coordinates[0]][coordinates[1]] = 0
-        else:
-            move = random.choice(neighbors.get(0))
-            grid[move[0]][move[1]] = 1
-            grid[coordinates[0]][coordinates[1]] = 0
-
-
-    # # Clear the screen
-    # screen.fill((255, 255, 255))
+    for row in range(grid_height):
+        for col in range(grid_width):
+            cell = grid[row][col]
+            neighbors = adjacent_neighbors((row, col))
+            live = len(neighbors[1])
+            if cell == 1:
+                if live == 0:
+                    new_grid[row][col] = 0
+                else:
+                    new_grid[row][col] = 1
+            else:
+                if live >= 3:
+                    new_grid[row][col] = 1
+                elif live == 1 and random.uniform(0, 1) <= 0.01:
+                    new_grid[row][col] = 1
+                elif live == 2 and random.uniform(0, 1) <= 0.1:
+                    new_grid[row][col] = 1
+                else:
+                    new_grid[row][col] = 0
 
     # Draw the grid
     for row in range(grid_height):
         for col in range(grid_width):
-            pygame.draw.rect(screen, COLORS[grid[row][col]], (col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE))
+            pygame.draw.rect(screen, COLORS[new_grid[row][col]], (col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE))
 
     pygame.display.flip()
+
+    grid = new_grid
 
 pygame.quit()
