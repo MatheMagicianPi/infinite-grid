e4427455c510e09d476edacfd380e8e1456b3b1e
Dang Phan <dangphan@uw.edu>
2023-11-12 17:00:28 -0800

    py

--- homework5/problem_3.py
+++ homework5/problem_3.py
@@ -17,12 +17,12 @@ p = Matrix([p1, p2, p3])
 q = Matrix([q1, q2, q3])
 
 # Substitute specific values for some variables
-p_values = {p1: 0, p2: 1}  # Assign specific values to p1 and p2
-q_values = {q1: 1, q2: 0}  # Assign specific values to q1 and q2
+p_values = {p1: 1, p2: 0}  # Assign specific values to p1 and p2
+q_values = {q1: 0, q2: 1}  # Assign specific values to q1 and q2
 
 # Calculate the dot product of q and A*p
-result = simplify(expand(p.dot(P*q).subs(p_values).subs(q_values)))
-result = simplify(expand(p.dot(P*q)))
+result = simplify(expand(q.dot(P*p).subs(p_values).subs(q_values)))
+result = simplify(expand(q.dot(P*p)))
 
 # Print the result
 print(f"{result}")
