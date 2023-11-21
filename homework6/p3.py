33c59a32b3310033c29794f84f696dfe9e3729ef
Dang Phan <dangphan@uw.edu>
2023-11-21 00:06:26 -0800

    final

--- homework6/p3.py
+++ homework6/p3.py
@@ -1,17 +1,24 @@
 from collections import Counter
 
 file_path = "homework6/cm.txt"
+
+# extract word frequencies in order
 with open(file_path, 'r') as file:
     words = file.read().split()
 word_freq = Counter(words)
 sorted_freqs = sorted(word_freq.values(), reverse=True)
+
+# number of distinct words found
 L = len(sorted_freqs)
 
-errors = []
 
+# find error of the 1/(x+3) estimate
+errors = []
 for i in range(L):
-    expected = L / (i + 3) # 1/(x+3) scaled to the number of words
-    error = abs(expected - sorted_freqs[i])
+    expected = L / (i + 3) # 1/(x+3) scaled to L
+    error = abs(expected - sorted_freqs[i]) # calculate error
     errors.append(round(error / L, 3)) # error as percentage of L
 
-print(errors)
+# print(errors)
+# print(errors[0:100])
+# print(max(errors[0:100]))
